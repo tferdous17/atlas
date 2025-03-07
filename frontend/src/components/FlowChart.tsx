ec6ebd9174c1fb056b6fbbc33ece581abacb4997
kyoshinoda <kyoshisew@gmail.com>
2025-03-07 16:51:54 -0500

    added flowChart

---
+++ frontend/src/components/FlowChart.tsx
@@ -0,0 +1,38 @@
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from "@xyflow/react";
+import { TooltipNode } from "@/components/tooltip-node"; // Ensure correct import
+import { initialEdges, initialNodes } from "@/utils/mockdata";
+const nodeTypes = {
+  tooltipNode: TooltipNode,
+};
+
+
+
+export default function FlowChart() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div className="w-full h-screen">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        fitView
+      />
+    </div>
+  );
+}
