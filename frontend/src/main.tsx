0aa23c6717eec31d7d49c74751fc374de85ad793
kyoshinoda <kyoshisew@gmail.com>
2025-03-07 22:32:05 -0500

    added react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import GraphPage from './pages/GraphPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<App />} />
+      <Route path='/graph' element={<GraphPage/>}/>
+    </Routes>
+  </BrowserRouter>
   </StrictMode>,
 )
---
+++ frontend/src/pages/GraphPage.tsx
@@ -0,0 +1,13 @@
+import GraphComponent from "@/components/GraphComponent";
+
+const GraphPage = () => {
+
+    return(
+        <div className="h-screen w-screen">
+            <GraphComponent/>
+        </div>
+    );
+
+}
+
+export default GraphPage;
