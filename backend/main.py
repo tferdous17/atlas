448863f7cc71b7d0c94011948753370d976d024e
tferdous17 <tferdous611@gmail.com>
2025-03-08 13:18:18 -0500

    implement caching for MVP testing

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,42 @@
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
-from typing import Annotated
+from typing import Annotated, Dict
 from generate_roadmap import generate_roadmap
 import uuid
 
 '''
 -POST /api/generateMap
 -GET  /api/roadmaps/<id>
-
 '''
 
-class Prompt(BaseModel):
-    text: str
-
 app = FastAPI()
+cache: Dict[str, dict] = {}
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+class Prompt(BaseModel):
+    text: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
+'''
+USE THIS JSON PROMPT FOR TESTING THE ENDPOINT:
+{
+    "text": "I want to build a full-stack Twitter clone using Vue.js."
+}
+'''
 @app.post("/api/generate-map")
 async def generate_map(prompt: Prompt):
+    if prompt.text in cache:
+        return cache[prompt.text]
+    
     response = await generate_roadmap(prompt.text)
     response["id"] = str(uuid.uuid4())
+    cache[prompt.text] = response
+    
     return response
 
 
